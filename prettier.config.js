cf9170dbf3285608e23f6f76527813a72090aae9
Tonkata-hub <tonyyankOV07@gmail.com>
2026-04-02 00:10:01 +0300

    added npm run format command

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 120,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "es5",
+	bracketSpacing: true,
+	arrowParens: "always",
+};
+
+export default config;
--- scripts/003_add_workout_name.sql
+++
@@ -1,2 +0,0 @@
--- Add optional name field to workouts (e.g. "Push Day", "Upper Body")
-ALTER TABLE workouts ADD COLUMN IF NOT EXISTS name TEXT;
